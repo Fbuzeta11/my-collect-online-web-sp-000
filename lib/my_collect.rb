4113c2d2281218abee7bd988ecc8fba75ec2a66b
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-01-13 02:20:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+list = ["ab", "cd", "ef"]
+
+def my_each(array)
+  i = 0
+  while i < array.length 
+    yield(array[i])
+      i = i + 1
+      
+  end
+  return array
+end
+
+my_each(list) {|i| "#{i}"}
 
 
